ac28d2cef26192a8253fc6fa01342964b091636a
Gabriel <gabriel.anguiano13x@gmail.com>
2023-02-25 18:38:42 -0600

    integrated basic login/logout with AuthO

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,7 +1,25 @@
+import { Auth0Provider } from "@auth0/auth0-react";
+import { LoginButton, LogoutButton } from "./components";
 import "./App.css";
 
 function App() {
-  return <div className="App">Hello World</div>;
+  const redirectLogin = "dev-tygwcsvuarfltlc8.us.auth0.com";
+  const clientID = "gWf16CFovrdZ0F0pIJ3Pa5EP1R9c3jyB";
+  return (
+    <Auth0Provider
+      domain={redirectLogin}
+      clientId={clientID}
+      authorizationParams={{
+        redirect_uri: window.location.origin,
+      }}
+    >
+      <div className="App">
+        Hello World
+        <LoginButton />
+        <LogoutButton />
+      </div>
+    </Auth0Provider>
+  );
 }
 
 export default App;
---
+++ front-end/src/components/index.tsx
@@ -0,0 +1,2 @@
+export { default as LoginButton } from "./loginBttn/LoginBttn";
+export { default as LogoutButton } from "./logoutBttn/LogoutBttn";
