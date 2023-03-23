9b9912d7519aff6031d4729ed24d1114c6ec1984
ProjectSkyapple <36686926+ProjectSkyapple@users.noreply.github.com>
2023-03-23 18:04:18 -0500

    Navbar fixes

--- src/components/adminDashboard/navbar/navbar.tsx
+++ src/components/adminDashboard/navbar/navbar.tsx
@@ -12,11 +12,11 @@ export default function BasicTabs() {
     <AppBar aria-label="nav bar" component="nav" position="sticky" color="primary" sx={{ display: "flex", flexDirection: "row", width: 1, p: 2, alignItems: "center" }}>
       <Typography variant="h6" sx={{ mx: 1 }}>Legacy<b>QC</b></Typography>
       <Box sx={{ flex: 1 }}>
-        <Button href="/performance" sx={{ color: "primary.contrastText", mx: 1 }} {...a11yProps(0)}>Performance</Button>
-        <Button href="/inspections" sx={{ color: "primary.contrastText", mx: 1 }} {...a11yProps(1)}>Inspections</Button>
-        <Button href="/schedules" sx={{ color: "primary.contrastText", mx: 1 }} {...a11yProps(2)}>Schedules</Button>
-        <Button href="/teammembers" sx={{ color: "primary.contrastText", mx: 1 }} {...a11yProps(3)}>Team Members</Button>
-        <Button href="/rooms" sx={{ color: "primary.contrastText", mx: 1 }} {...a11yProps(4)}>Rooms</Button>
+        <Button href={"/performance"} sx={{ color: "primary.contrastText", mx: 1 }} {...a11yProps(0)}>Performance</Button>
+        <Button href={"/inspections"} sx={{ color: "primary.contrastText", mx: 1 }} {...a11yProps(1)}>Inspections</Button>
+        <Button href={"/schedules"} sx={{ color: "primary.contrastText", mx: 1 }} {...a11yProps(2)}>Schedules</Button>
+        <Button href={"/teammembers"} sx={{ color: "primary.contrastText", mx: 1 }} {...a11yProps(3)}>Team Members</Button>
+        <Button href={"/rooms"} sx={{ color: "primary.contrastText", mx: 1 }} {...a11yProps(4)}>Rooms</Button>
       </Box>
       <Box>
         <Avatar sx={{ bgcolor: "secondary.main" }}>U</Avatar>
