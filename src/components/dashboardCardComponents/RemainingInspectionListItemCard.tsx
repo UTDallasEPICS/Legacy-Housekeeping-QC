56eca5d98c86147dd710c136551104ebe2e94a29
ProjectSkyapple <36686926+ProjectSkyapple@users.noreply.github.com>
2023-03-30 04:09:14 -0500

    Create RemainingInspectionListItemCard.tsx

---
+++ src/components/dashboardCardComponents/RemainingInspectionListItemCard.tsx
@@ -0,0 +1,25 @@
+import { Box, Card, CardActionArea, CardActions, ListItem, Typography, Button, CardContent } from "@mui/material";
+import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
+
+const RemainingInspectionListItemCard = ({ buildingId, roomId, teamMemberName, timeCleaned, cleaningType }) => {
+    return (
+        <ListItem>
+            <Card variant="outlined" sx={{ width: 1 }}>
+                <Box sx={{ display: "inline-flex", width: 1, alignItems: "center" }}>
+                    <CardActionArea>
+                        <CardContent sx={{ width: 1, mr: 1 }}>
+                            <Typography variant="h6"><b>{roomId} </b>in {buildingId}</Typography>
+                            <Typography>{cleaningType} Cleaning</Typography>
+                            <Typography>Cleaned by {teamMemberName} at {timeCleaned}</Typography>
+                        </CardContent>
+                    </CardActionArea>
+                    <CardActions sx={{ ml: 1 }}>
+                        <Button> endButton={<ArrowForwardIcon />}Inspect</Button>
+                    </CardActions>
+                </Box>
+            </Card>
+        </ListItem>
+    );
+};
+
+export default RemainingInspectionListItemCard;
