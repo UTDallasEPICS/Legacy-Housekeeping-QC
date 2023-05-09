72d34d76e4959b78caa3f7f40b850274b5256012
joelvarghese3 <109166209+joelvarghese3@users.noreply.github.com>
2023-05-08 19:48:53 -0500

    Update individual_performance.tsx

--- pages/individual_performance.tsx
+++ pages/individual_performance.tsx
@@ -1,6 +1,9 @@
+import { Navbar} from "../src/components";
+
 const individual_performance = () => {
     return (
       <div>
+        <Navbar />
         <div style={{textAlign: 'center'}}>
         <h1>Team Member 1</h1>
         <h3>07/21/22 - 08/21/22</h3>
@@ -15,35 +18,35 @@ const individual_performance = () => {
               </td>
             </tr>
             <tr>
-              <td style={{ textAlign: "center" }}>07/22/22</td>
+              <td style={{ textAlign: "center" }}>07/24/22</td>
               <td style={{ textAlign: "center" }}>Avg: 95%</td>
               <td style={{ textAlign: "center" }}>
                 <a href="/report_performance">View Report</a>
               </td>
             </tr>
             <tr>
-              <td style={{ textAlign: "center" }}>07/23/22</td>
+              <td style={{ textAlign: "center" }}>07/29/22</td>
               <td style={{ textAlign: "center" }}>Avg: 91%</td>
               <td style={{ textAlign: "center" }}>
                 <a href="/report_performance">View Report</a>
               </td>
             </tr>
             <tr>
-              <td style={{ textAlign: "center" }}>07/24/22</td>
+              <td style={{ textAlign: "center" }}>08/03/22</td>
               <td style={{ textAlign: "center" }}>Avg: 89%</td>
               <td style={{ textAlign: "center" }}>
                 <a href="/report_performance">View Report</a>
               </td>
             </tr>
             <tr>
-              <td style={{ textAlign: "center" }}>07/25/22</td>
+              <td style={{ textAlign: "center" }}>08/09/22</td>
               <td style={{ textAlign: "center" }}>Avg: 94%</td>
               <td style={{ textAlign: "center" }}>
                 <a href="/report_performance">View Report</a>
               </td>
             </tr>
             <tr>
-              <td style={{ textAlign: "center" }}>07/26/22</td>
+              <td style={{ textAlign: "center" }}>08/20/22</td>
               <td style={{ textAlign: "center" }}>Avg: 100%</td>
               <td style={{ textAlign: "center" }}>
                 <a href="/report_performance">View Report</a>
