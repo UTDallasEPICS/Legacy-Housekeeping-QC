900e5a2b3d59985fd02d3352b6e4e0710b8ab477
joelvarghese3 <109166209+joelvarghese3@users.noreply.github.com>
2023-05-08 19:46:24 -0500

    Update members_performance.tsx

--- pages/members_performance.tsx
+++ pages/members_performance.tsx
@@ -1,67 +1,71 @@
-import { useState } from 'react';
+import { Navbar} from "../src/components";
 
-const members_performance = () => {
-  const [activeButton, setActiveButton] = useState('');
-
-  const handleButtonClick = (buttonName) => {
-    setActiveButton(buttonName);
-  };
-
+const performance = () => {
   return (
-      
-    <div style={{ display: 'flex', height: '100vh' }}>
-      <div style={{ flex: 1, backgroundColor: '#ccc', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
-        <button style={{ width: '100%', height: '25%', fontSize: '2rem', margin: '1rem 0',  backgroundColor: activeButton === 'Member 1' ? '#fff' : '#ccc', outline: activeButton === 'Member 1' ? '2px solid black' : 'none' }} onClick={() => handleButtonClick('Member 1')}>Member 1</button>
-        <button style={{ width: '100%', height: '25%', fontSize: '2rem', margin: '1rem 0',  backgroundColor: activeButton === 'Member 2' ? '#fff' : '#ccc', outline: activeButton === 'Member 2' ? '2px solid black' : 'none' }} onClick={() => handleButtonClick('Member 2')}>Member 2</button>
-        <button style={{ width: '100%', height: '25%', fontSize: '2rem', margin: '1rem 0',  backgroundColor: activeButton === 'Member 3' ? '#fff' : '#ccc', outline: activeButton === 'Member 3' ? '2px solid black' : 'none' }} onClick={() => handleButtonClick('Member 3')}>Member 3</button>
-        <button style={{ width: '100%', height: '25%', fontSize: '2rem', margin: '1rem 0',  backgroundColor: activeButton === 'Member 4' ? '#fff' : '#ccc', outline: activeButton === 'Member 4' ? '2px solid black' : 'none' }} onClick={() => handleButtonClick('Member 4')}>Member 4</button>
+    <div>
+      <Navbar />
+
+      <div style={{ backgroundColor: '#3498db', width: '200px', height: '50px', display: 'flex', justifyContent: 'center', alignItems: 'center', border: '1px solid'}}>
+        <a href="/individual_performance">One Month Score</a>
       </div>
-    
+
+      <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start', marginTop: '20px' }}>
+        <button style={{ backgroundColor: '#3498db', width: '150px', height: '150px', borderRadius: '10px', border: 'none', cursor: 'pointer', marginBottom: '10px' }}>Member 1</button>
+        <button style={{ backgroundColor: '#3498db', width: '150px', height: '150px', borderRadius: '10px', border: 'none', cursor: 'pointer', marginBottom: '10px' }}>Member 2</button>
+        <button style={{ backgroundColor: '#3498db', width: '150px', height: '150px', borderRadius: '10px', border: 'none', cursor: 'pointer', marginBottom: '10px' }}>Member 3</button>
+        <button style={{ backgroundColor: '#3498db', width: '150px', height: '150px', borderRadius: '10px', border: 'none', cursor: 'pointer', marginBottom: '10px' }}>Member 4</button>
+      </div>
+
+
       <div style={{ flex: 3, backgroundColor: '#fff', padding: '2rem' }}>
-        <h1>Member 1</h1>
-        <h4>Avg Score: 81</h4>
       
-        <div style={{ position: 'absolute', top: '400px', left: '400px'}}>
+
+
+        <div style={{ position: 'absolute', top: '150px', left: '200px'}}>
+                <h1>Member 1</h1>
+                <h4>Avg Score: 81</h4>
+        </div>
+
+
+      
+        <div style={{ position: 'absolute', top: '400px', left: '200px'}}>
         <h3>Score History</h3> 
         <p>Top 3 Scores:</p>
         </div>
         
         </div>
-        <div style={{ position: 'absolute', top: '470px', left: '400px'}}>
+        <div style={{ position: 'absolute', top: '470px', left: '200px'}}>
         <p>08/21/23: 100</p>
         </div>
 
-        <div style={{ position: 'absolute', top: '490px', left: '400px'}}>
+        <div style={{ position: 'absolute', top: '490px', left: '200px'}}>
         <p>08/27/23: 89</p>
         </div>
 
-        <div style={{ position: 'absolute', top: '510px', left: '400px'}}>
+        <div style={{ position: 'absolute', top: '510px', left: '200px'}}>
         <p>08/06/23: 87</p>
         <p>Lowest 3 Scores:</p>
         </div>
 
-        <div style={{ position: 'absolute', top: '570px', left: '400px'}}>
+        <div style={{ position: 'absolute', top: '570px', left: '200px'}}>
         <p>08/28/23: 79</p>
         </div>
 
-        <div style={{ position: 'absolute', top: '590px', left: '400px'}}>
+        <div style={{ position: 'absolute', top: '590px', left: '200px'}}>
         <p>08/01/23: 78</p>
         </div>
 
-        <div style={{ position: 'absolute', top: '610px', left: '400px'}}>
+        <div style={{ position: 'absolute', top: '610px', left: '200px'}}>
             <p>08/17/23: 69</p>
         </div>
         
-        <div style={{ backgroundColor: '#ccc', width: '200px', height: '50px', display: 'flex', justifyContent: 'center', alignItems: 'center', border: '1px solid'}}>
-        <a href="/individual_performance">One Month Score</a>
-        </div>
         
 
         <div style={{ position: 'absolute', top: '400px', left: '1100px'}}>
         <h3>Trend Data:</h3> 
         </div>
         
-        <div style={{ position: 'absolute', bottom: '70px', right: '200px' }}>
+        <div style={{ position: 'absolute', bottom: '70px', right: '85px' }}>
         <div style={{ backgroundColor: '#fff', width: '150px', height: '40px', display: 'flex', justifyContent: 'center', alignItems: 'center' , border: '1px solid'}}>
         <p>Floors 100%</p>
         </div>
@@ -76,7 +80,7 @@ const members_performance = () => {
         </div>
         </div>
 
-        <div style={{ position: 'absolute', bottom: '50px', right: '150px', display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+        <div style={{ position: 'absolute', bottom: '50px', right: '200px', display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
     <img src="https://quomodocumque.files.wordpress.com/2013/07/stacksgraph_inversesquare.png" style={{ width: '50%', height: 'auto' }} />
     </div>
 
@@ -84,8 +88,10 @@ const members_performance = () => {
 
     
     </div>
+
+
+    
   );
 };
-  
-  
-  export default members_performance;
+
+export default performance;
