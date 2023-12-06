b9186cd78476bf7507fd269229c9d607d818d8c3
ishavietla <85516016+ishavietla@users.noreply.github.com>
2023-12-06 14:51:50 -0600

    Team Members Page Update

--- pages/api/user/validate.ts
+++ pages/api/user/validate.ts
@@ -7,35 +7,37 @@ export default async function handler(
   res: NextApiResponse
 ) {
   try {
-    if (req.method === "POST") {
-      const { email, password } = req.body;
-
-      const userExist = await prisma.user.findUnique({
-        where: { email: email },
-        select: {
-          password: true,
-        },
-      });
-
-      // Check if user exists if not return
-      if (!userExist) res.status(500).send({ error: "User not found" });
-
-      // Check if password matches if not return
-      if (!(await bcrypt.compare(password, userExist.password)))
-        res.status(500).send({ error: "Invalid password" });
-
-        const user = await prisma.user.findUnique({
-        where: { email: email },
-        select: {
-          first_name: true,
-          role: true,
-          email: true,
-        },
-      });
-      
-      res.status(200).json(user);
+    if (req.method !== "POST") {
+      return res.status(405).send({ error: "Method not allowed" });
     }
+
+    const { email, password } = req.body;
+
+    const userExist = await prisma.user.findUnique({
+      where: { email: email },
+      select: {
+        password: true,
+      },
+    });
+
+    // Check if user exists if not return
+    if (!userExist) return res.status(404).send({ error: "User not found" });
+
+    // Check if password matches if not return
+    const isPasswordMatch = await bcrypt.compare(password, userExist.password);
+    if (!isPasswordMatch) return res.status(401).send({ error: "Invalid password" });
+
+    const user = await prisma.user.findUnique({
+      where: { email: email },
+      select: {
+        first_name: true,
+        role: true,
+        email: true,
+      },
+    });
+
+    return res.status(200).json(user);
   } catch (error) {
-    res.status(500).send(error + " :Error updating user");
+    return res.status(500).send({ error: `Error updating user: ${error.message}` });
   }
 }
