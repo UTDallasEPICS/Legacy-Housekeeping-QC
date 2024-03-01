7ea085e2b2b11ea2cb663aac1f37566694057d0e
Vivek Madarapu <vxm200045@utdallas.edu>
2024-03-01 11:31:35 -0600

    Fixed prisma migration error

--- prisma/migrations/20240227233833_revised_schema_2_27/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - Made the column `score` on table `roomreport` required. This step will fail if there are existing NULL values in that column.
-
-*/
--- AlterTable
-ALTER TABLE `roomreport` MODIFY `date` DATETIME(3) NULL DEFAULT CURRENT_TIMESTAMP(3),
-    MODIFY `score` INTEGER NOT NULL;
