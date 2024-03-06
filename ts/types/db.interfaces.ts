259a3558203ad80c9808ca249463385fafa00fd8
RussianMeat <iamtenvoltz@gmail.com>
2024-03-05 22:58:52 -0600

    Add more type conversion for database output

--- ts/types/db.interfaces.ts
+++ ts/types/db.interfaces.ts
@@ -10,8 +10,8 @@ import {
   HollisticRubric as HollisticRubricDB,
   QuantitativeRubric as QuantitativeRubricDB,
 } from "@prisma/client";
-export type TeamMember = Omit<TeamMemberDB & PersonDB, "type">;
-export type User = Omit<UserDB & PersonDB, "type">;
+export type TeamMember = Omit<TeamMemberDB & PersonDB, "type" | "person_id">;
+export type User = Omit<UserDB & PersonDB, "type" | "person_id">;
 
 export type CommonArea = Omit<CommonAreaDB & RoomDB, "type">;
 export type PersonalRoom = Omit<PersonalRoomDB & RoomDB, "type">;
@@ -19,41 +19,75 @@ export type PersonalRoom = Omit<PersonalRoomDB & RoomDB, "type">;
 export type HollisticRubric = Omit<HollisticRubricDB & RubricDB, "type">;
 export type QuantitativeRubric = Omit<QuantitativeRubricDB & RubricDB, "type">;
 
-const teamMemberPerson = Prisma.validator<Prisma.TeamMemberArgs>()({
+const personIncludeTeamMember = Prisma.validator<Prisma.PersonArgs>()({
+  include: { teamMember: true },
+});
+type PersonIncludeTeamMember = Prisma.PersonGetPayload<
+  typeof personIncludeTeamMember
+>;
+const teamMemberIncludePerson = Prisma.validator<Prisma.TeamMemberArgs>()({
   include: { person: true },
 });
-type TeamMemberPerson = Prisma.TeamMemberGetPayload<typeof teamMemberPerson>;
-export function toTeamMember(a: TeamMemberPerson): TeamMember {
+type TeamMemberIncludePerson = Prisma.TeamMemberGetPayload<
+  typeof teamMemberIncludePerson
+>;
+export function toTeamMember(a: PersonIncludeTeamMember): TeamMember {
   return {
     id: a.id,
+    first_name: a.first_name,
+    last_name: a.last_name,
+    email: a.teamMember.email,
+    country_code: a.country_code,
+    state_code: a.state_code,
+    phone_number: a.phone_number,
+  };
+}
+export function fromTeamMember(a: TeamMemberIncludePerson): TeamMember {
+  return {
+    id: a.person.id,
     first_name: a.person.first_name,
     last_name: a.person.last_name,
-    email: a.person.email,
+    email: a.email,
     country_code: a.person.country_code,
     state_code: a.person.state_code,
     phone_number: a.person.phone_number,
-    person_id: a.person_id,
   };
 }
 
-const userPerson = Prisma.validator<Prisma.UserArgs>()({
+const personIncludeUser = Prisma.validator<Prisma.PersonArgs>()({
+  include: { user: true },
+});
+type PersonIncludeUser = Prisma.PersonGetPayload<typeof personIncludeUser>;
+const userIncludePerson = Prisma.validator<Prisma.UserArgs>()({
   include: { person: true },
 });
-type UserPerson = Prisma.UserGetPayload<typeof userPerson>;
-export function toUser(a: UserPerson): User {
+type UserIncludePerson = Prisma.UserGetPayload<typeof userIncludePerson>;
+export function toUser(a: PersonIncludeUser): User {
   return {
     id: a.id,
+    first_name: a.first_name,
+    last_name: a.last_name,
+    email: a.user.email,
+    password: a.user.password,
+    country_code: a.country_code,
+    state_code: a.state_code,
+    phone_number: a.phone_number,
+  };
+}
+export function fromUser(a: UserIncludePerson): User {
+  return {
+    id: a.person.id,
     first_name: a.person.first_name,
     last_name: a.person.last_name,
-    email: a.person.email,
+    email: a.email,
+    password: a.password,
     country_code: a.person.country_code,
     state_code: a.person.state_code,
     phone_number: a.person.phone_number,
-    username: a.username,
-    password: a.password,
-    person_id: a.person_id,
   };
 }
+
+/* Need to reverse the direction of the type
 
 const commonAreaRoom = Prisma.validator<Prisma.CommonAreaArgs>()({
   include: { room: true },
@@ -110,3 +144,5 @@ export function toQuantitativeRubric(
     rubric_id: a.rubric_id,
   };
 }
+
+*/
