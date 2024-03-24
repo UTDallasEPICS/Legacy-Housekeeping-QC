3aca733416ab3dd1f3e1d52efbc2288c317c3c73
RussianMeat <iamtenvoltz@gmail.com>
2024-03-23 22:59:12 -0500

    Create API to modify items in a room

---
+++ ts/const/rubric.constant.ts
@@ -0,0 +1,47 @@
+export const defaultRequirements = [
+  {
+    category: "Weekly Service",
+    items: [
+      "All flooring is swept, mopped, vacuumed.",
+      "All window sills and window ledges are wiped down.",
+    ],
+  },
+  {
+    category: "Additional Service",
+    items: ["Clean and defrost freezer", "Clean exhaust fan/hood filter"],
+  },
+];
+export const defaultItems = [
+  {
+    category: "General",
+    items: [
+      "Telephone",
+      "Overbed Table",
+      "Bedside Table",
+      "Call Button",
+      "Bed",
+      "Trash Can",
+      "Television",
+      "Walls",
+      "Corners & Edges",
+      "Doors",
+      "Floors",
+    ],
+  },
+  {
+    category: "Bathroom",
+    items: [
+      "Sink",
+      "Walls",
+      "Mirrors",
+      "Dispensers",
+      "Tub/Shower",
+      "Toilet",
+      "Floors",
+    ],
+  },
+  {
+    category: "Dusting",
+    items: ["Low Dusting (<6')", "High Dusting (>6')"],
+  },
+];
