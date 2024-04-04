86212b18f031b821c49979e63a5829eeecc0bfa5
RussianMeat <iamtenvoltz@gmail.com>
2024-04-04 15:47:50 -0500

    Move some function down the card fo better reusability

--- src/components/inspections/Card/completedCard.tsx
+++ src/components/inspections/Card/completedCard.tsx
@@ -9,18 +9,24 @@ import {
 } from "@mui/material";
 import Link from "next/link";
 import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
-import { InspectionCardProps } from "../../../../ts/interfaces/roomReport.interfaces";
+import {
+  InspectionCardProps,
+  toCompletedInspectionCardProps,
+} from "../../../../ts/interfaces/roomReport.interfaces";
 import { useDispatch } from "react-redux";
 import { setInspectionSelectionData } from "../../../../slices/inspectionSelectionSlice";
 import { useRouter } from "next/router";
+import { Inspection } from "../../../../ts/types/db.interfaces";
 
 const UncompletedCard = ({
   card_id,
-  inspectionProps,
+  inspection,
 }: {
   card_id: number;
-  inspectionProps: InspectionCardProps;
+  inspection: Inspection;
 }) => {
+  const inspectionProps: InspectionCardProps =
+    toCompletedInspectionCardProps(inspection);
   const router = useRouter();
   const dispatch = useDispatch();
   const handleClicked = () => {
