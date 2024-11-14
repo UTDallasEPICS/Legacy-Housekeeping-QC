957af7adc279cde5f3026a8df5ee31ec38d247a1
Dennis Keithly <dxk200017@utdallas.edu>
2024-11-14 15:21:33 -0600

    Add test buttons to the sign in page. Not working Help

--- pages/api/auth/signin.tsx
+++ pages/api/auth/signin.tsx
@@ -24,6 +24,8 @@ interface SignInProps {
 }
 
 const SignIn = ({ providers }: SignInProps) => {
+  if (!providers) return <p> HELP </p>;
+
   return (
     <Container>
       <Box
--- src/components/authsComponents/signInCardInVPSize/signInCardInVPSize.tsx
+++ src/components/authsComponents/signInCardInVPSize/signInCardInVPSize.tsx
@@ -127,9 +127,23 @@ export default signInCardInVPSize;
 
 import { Box, Button, Card, Container, Stack, Typography } from "@mui/material";
 import Link from "next/link";
+import { useUser } from "@auth0/nextjs-auth0/client";
+import { useRouter } from "next/router";
 import { signIn } from "next-auth/react";
+import { signOut } from "next-auth/react";
+import { useEffect } from "react";
 
 const SignInCardInVPSize = () => {
+  const { user, error, isLoading } = useUser();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (user) {
+      console.log("User is signed in");
+      router.push("/admin/adminDashboard");
+    }
+  }, [user, router]);
+
   return (
     <Container>
       <Box
@@ -149,9 +163,28 @@ const SignInCardInVPSize = () => {
               <Typography variant="h5">Sign In</Typography>
               <Button
                 variant="contained"
-                onClick={() => signIn("auth0")}
+                onClick={() => {
+                  console.log("Signing in with Auth0");
+                  signIn("auth0");
+                }}
               >
                 Login with Auth0
+              </Button>
+              <Button
+                variant="contained"
+                onClick={() => {
+                  console.log("Current user session:", user);
+                }}
+              >
+                Test User
+              </Button>
+              <Button
+                variant="contained"
+                onClick={() => {
+                  router.push("/admin/adminDashboard");
+                }}
+              >
+                Test Push
               </Button>
               <Link href="/auths/signup">Sign up</Link>
             </Stack>
