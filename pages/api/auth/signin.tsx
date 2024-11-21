376a4dd6756667811f4f6562809b66b48c1105f4
Akshay Nagarajan <akshay.h.nagarajan@gmail.com>
2024-11-21 16:43:41 -0600

    Quick update to commenting auth0

--- pages/api/auth/signin.tsx
+++ pages/api/auth/signin.tsx
@@ -76,29 +76,29 @@ import { withPageAuthRequired } from "@auth0/nextjs-auth0";
 import { useUser } from "@auth0/nextjs-auth0/client";
 import { initAuth0 } from '@auth0/nextjs-auth0';
 
-const auth0 = initAuth0({
-    domain: process.env.AUTH0_DOMAIN,
-    clientId: process.env.AUTH0_CLIENT_ID,
-    clientSecret: process.env.AUTH0_CLIENT_SECRET,
-    scope: 'openid profile',
-    redirectUri: process.env.REDIRECT_URI,
-    postLogoutRedirectUri: process.env.POST_LOGOUT_REDIRECT_URI,
-    session: {
-        cookieSecret: process.env.SESSION_COOKIE_SECRET,
-        cookieLifetime: 7200,
-        storeIdToken: false,
-        storeAccessToken: false,
-        storeRefreshToken: false
-    }
-});
+// const auth0 = initAuth0({
+//     domain: process.env.AUTH0_DOMAIN,
+//     clientId: process.env.AUTH0_CLIENT_ID,
+//     clientSecret: process.env.AUTH0_CLIENT_SECRET,
+//     scope: 'openid profile',
+//     redirectUri: process.env.REDIRECT_URI,
+//     postLogoutRedirectUri: process.env.POST_LOGOUT_REDIRECT_URI,
+//     session: {
+//         cookieSecret: process.env.SESSION_COOKIE_SECRET,
+//         cookieLifetime: 7200,
+//         storeIdToken: false,
+//         storeAccessToken: false,
+//         storeRefreshToken: false
+//     }
+// });
 
-export default async function signin(req, res) {
-    try {
-        await auth0.handleLogin(req, res);
-    } catch (error) {
-        res.status(error.status || 400).end(error.message);
-    }
-}
+// export default async function signin(req, res) {
+//     try {
+//         await auth0.handleLogin(req, res);
+//     } catch (error) {
+//         res.status(error.status || 400).end(error.message);
+//     }
+// }
 
 const SignIn = () => {
   const { user, isLoading } = useUser(); 
