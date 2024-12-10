4efdef2b3b17d81c62586e38c30a91251c69c548
Akshay H Nagarajan <akshay.h.nagarajan@gmail.com>
2024-12-09 22:16:14 -0600

    Add floors property to InspectionPlannerProps interface

--- src/components/inspections/Planner/InspectionPlanner/props.ts
+++ src/components/inspections/Planner/InspectionPlanner/props.ts
@@ -4,4 +4,5 @@ import { TeamMember } from "../../../../../ts/types/db.interfaces";
 export interface InspectionPlannerProps {
   members: TeamMember[];
   buildings: BuildingWithRooms[];
+  floors: BuildingsWithFloors[];
 }
